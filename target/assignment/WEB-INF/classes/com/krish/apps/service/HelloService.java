2c855c47c84af2a10fe4cec99a84cb1f451f76a2
krishanrathi <krishanrathi79@gmail.com>
2014-12-01 01:18:15 -0500

    Firt version pushed Signed-off-by: krishanrathi <krishanrathi79@gmail.com>

---
+++ target/assignment/WEB-INF/classes/com/krish/apps/service/HelloService.java
@@ -0,0 +1,13 @@
+package com.krish.apps.service;
+@Path("/hello")
+public class HelloService {
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+}
